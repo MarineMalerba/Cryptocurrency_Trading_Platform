0349b0948d90a60431237c3b8611b5a69201016a
MarineMalerba <159458126+MarineMalerba@users.noreply.github.com>
2024-07-28 14:58:32 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "Wallet.h"
+#include <iostream>
+#include "MerkelMain.h"
+
+int main()
+{   
+    MerkelMain app{};
+    app.init();
+    
+}
+
+// cd /Users/marinemalerba/Documents/PROGRAMMATION/C++/src
+// g++ -std=c++11 *.cpp
+// ./a.out
